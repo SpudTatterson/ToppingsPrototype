9269b3b013ac22b06996d84d045251d7e26d74e1
Aviv <avivhauyn@gmail.com>
2024-04-02 01:36:18 +0300

    Made new Level Picker

--- Assets/_Scripts/UI/LoadStarCount.cs
+++ Assets/_Scripts/UI/LoadStarCount.cs
@@ -1,8 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadStarCount : MonoBehaviour
 {
-    
+    [SerializeField] int levelBuildIndex;
+    void Start()
+    {
+        string bestScoreKey = "BestScoreScene" + levelBuildIndex.ToString();
+        float starCount = PlayerPrefs.GetFloat(bestScoreKey, 0);
+        GetComponent<Image>().fillAmount = starCount;
+    }
 }
