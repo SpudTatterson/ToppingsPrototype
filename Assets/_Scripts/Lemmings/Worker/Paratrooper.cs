4c402489d497fd1c79b77559706a38334ce8f13a
Aviv <avivhauyn@gmail.com>
2024-04-10 14:12:46 +0300

    Added Paratrooper class

---
+++ Assets/_Scripts/Lemmings/Worker/Paratrooper.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Paratrooper : Worker
+{
+    [Header("Worker Settings")]
+    [SerializeField] float parachuteFloat;
+    [SerializeField][Range(1.01f, 1.1f)] private float velocityStop;
+    LemmingMovement movement;
+    float timer;
+    void Start()
+    {
+        movement = GetComponentInParent<LemmingMovement>();
+
+    }
+    public override void WorkerLogic()
+    {
+        
+        var rb = GetComponentInParent<Rigidbody>();
+
+        if (movement.isGrounded == false && rb.velocity.y < -parachuteFloat)
+        {
+            timer += Time.fixedDeltaTime;
+            rb.velocity = rb.velocity / velocityStop;
+            Vector3 newVelocity = rb.velocity;
+            newVelocity.y = -parachuteFloat;
+            if (rb.velocity.y < -parachuteFloat) { rb.velocity = newVelocity; }
+            // Insert parachute animation here.
+        }
+        else if (movement.isGrounded == true && timer > 0)
+        {
+            timer = 0;
+            this.enabled = false;
+        }
+    }
+}
