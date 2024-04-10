fbf79a80f3f0ed30ca5e427e0fa11e5a1bf1e40b
Aviv <avivhauyn@gmail.com>
2024-04-10 14:14:14 +0300

    Updated job selector to make sure minion has 1 job

--- Assets/_Scripts/Lemmings/JobsSelector.cs
+++ Assets/_Scripts/Lemmings/JobsSelector.cs
@@ -11,28 +11,48 @@ public class JobsSelector : MonoBehaviour
     {
         if (!toggle) return;
 
-        if(Input.GetKeyDown(KeyCode.Mouse1))
+        if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-            toggle = false;
-            type = WorkerType.Default;
+            Reset();
             Debug.Log("cancel");
         }
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if(Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, minionLayer, QueryTriggerInteraction.Ignore) && Input.GetKeyDown(KeyCode.Mouse0))
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, minionLayer, QueryTriggerInteraction.Ignore) && Input.GetKeyDown(KeyCode.Mouse0))
         {
+            if (hit.transform.gameObject.GetComponentInChildren<Worker>().CheckIfAlreadyWorking()) return;
             Debug.Log("working");
-            if(type == WorkerType.WoodWorker)
-            hit.transform.gameObject.GetComponentInChildren<WoodWorker>().enabled = true;
+            if (type == WorkerType.WoodWorker)
+            {
+                hit.transform.gameObject.GetComponentInChildren<WoodWorker>().enabled = true;
+                Reset();
+            }
+
+            if (type == WorkerType.Shield)
+            {
+                hit.transform.gameObject.GetComponentInChildren<Shield>().enabled = true;
+                Reset();
+            }
         }
 
 
+    }
+
+    private void Reset()
+    {
+        toggle = false;
+        type = WorkerType.Default;
     }
 
     public void SetWoodWorker()
     {
         toggle = true;
         type = WorkerType.WoodWorker;
+    }
+    public void SetShieldWorker()
+    {
+        toggle = true;
+        type = WorkerType.Shield;
     }
 }
