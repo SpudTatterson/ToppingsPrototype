0ec2a579017df301e5ecec4f93e29fb0611435f8
Aviv <avivhauyn@gmail.com>
2024-04-10 14:45:14 +0300

    Changed cast type to sphere for comfort

--- Assets/_Scripts/Lemmings/JobsSelector.cs
+++ Assets/_Scripts/Lemmings/JobsSelector.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class JobsSelector : MonoBehaviour
 {
     [SerializeField] LayerMask minionLayer;
+    [SerializeField] float castRadius = 0.5f;
     bool toggle = false;
     WorkerType type;
     void Update()
@@ -19,21 +20,21 @@ public class JobsSelector : MonoBehaviour
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, minionLayer, QueryTriggerInteraction.Ignore) && Input.GetKeyDown(KeyCode.Mouse0))
+        if (Physics.SphereCast(ray.origin, castRadius, ray.direction, out hit, Mathf.Infinity, minionLayer, QueryTriggerInteraction.Ignore) && Input.GetKeyDown(KeyCode.Mouse0))
         {
             Worker worker = hit.transform.gameObject.GetComponentInChildren<Worker>();
             if (worker.CheckIfAlreadyWorking()) return;
             Debug.Log("working");
-            
+
             List<Worker> workers = worker.GetWorkers();
             foreach (Worker workScript in workers)
             {
-                if(workScript.workerType == type)
-                    {
-                        workScript.enabled = true;
-                        Reset();
-                        return;
-                    }
+                if (workScript.workerType == type)
+                {
+                    workScript.enabled = true;
+                    Reset();
+                    return;
+                }
 
             }
         }
