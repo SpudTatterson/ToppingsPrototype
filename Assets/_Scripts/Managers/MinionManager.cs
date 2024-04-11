a723629dc52d6cad1a71bc81c80d31d2e2b69bb5
Aviv <avivhauyn@gmail.com>
2024-04-11 23:15:34 +0300

    Changed var name

--- Assets/_Scripts/Managers/MinionManager.cs
+++ Assets/_Scripts/Managers/MinionManager.cs
@@ -13,13 +13,13 @@ public class MinionManager : MonoBehaviour
         else
             Debug.LogError("More Then one minion manager exists please remove one");
     }
-    public void Add(LemmingHealth movement)
+    public void Add(LemmingHealth minion)
     {
-        minions.Add(movement);
+        minions.Add(minion);
     }
-    public void Remove(LemmingHealth movement)
+    public void Remove(LemmingHealth minion)
     {
-        minions.Remove(movement);
+        minions.Remove(minion);
         CheckWinState();
     }
     void CheckWinState()
@@ -28,7 +28,6 @@ public class MinionManager : MonoBehaviour
         Debug.Log(minions.Count.ToString());
         if(minions.Count == 0 && LevelEndPoint.instance.GetPassedMinionCount() == 0)
         {
-
             VictoryManager.instance.TriggerLoss();
         }
         else if(minions.Count == 0 && LevelEndPoint.instance.CheckForVictory())
