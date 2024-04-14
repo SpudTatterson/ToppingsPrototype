3be716f3e3d80f9952048ac3675c66e44cb5aa74
Aviv <avivhauyn@gmail.com>
2024-04-14 13:59:50 +0300

    added new minion customizer system

---
+++ Assets/_Scripts/Lemmings/MinionCustomizer.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionCustomizer : MonoBehaviour
+{
+    public ClothingSet defaultClothing;
+
+    BoneContainer bones;
+
+    List<GameObject> oldClothes = new List<GameObject>();
+    void Awake()
+    {
+        bones = GetComponent<BoneContainer>();
+        UpdateClothing(defaultClothing);
+    }
+
+    public void UpdateClothing(ClothingSet clothingSet)
+    {
+        if (oldClothes.Count != 0)
+        {
+            foreach (GameObject cloth in oldClothes)
+            {
+                Destroy(cloth);
+            }
+        }
+        if (clothingSet.hat != null)
+        {
+            GameObject hat = Instantiate(clothingSet.hat, bones.head.position, Quaternion.identity, bones.head);
+            hat.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            oldClothes.Add(hat);
+        }
+        if(clothingSet.backpack != null)
+        {
+            GameObject backpack = Instantiate(clothingSet.backpack, bones.back.position, Quaternion.identity, bones.back);
+            oldClothes.Add(backpack);
+        }
+        if(clothingSet.handItem != null)
+        {
+            GameObject handItem = Instantiate(clothingSet.handItem, bones.hand.position, Quaternion.identity, bones.hand);
+            handItem.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            oldClothes.Add(handItem);
+        }
+    }
+}
