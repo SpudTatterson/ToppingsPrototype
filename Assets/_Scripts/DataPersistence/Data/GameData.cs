b01adffafd2a105ec97371b110204a751358ed9a
Aviv <avivhauyn@gmail.com>
2024-04-15 13:13:52 +0300

    Added a method to get LevelData from buildIndex

--- Assets/_Scripts/DataPersistence/Data/GameData.cs
+++ Assets/_Scripts/DataPersistence/Data/GameData.cs
@@ -14,7 +14,7 @@ public class SettingsData : Data
     public float masterVolume;
     public float fxVolume;
     public float musicVolume;
-    
+
     public SettingsData()
     {
         this.masterVolume = 1;
@@ -27,6 +27,29 @@ public class GameData : Data
 {
     public List<LevelDataSer> levels = new List<LevelDataSer>();
 
+    public LevelDataSer GetLevelData(int buildIndex)
+    {
+        if (levels.Count > 1)
+        {
+            foreach (LevelDataSer data in levels)
+            {
+                if (data.levelID == buildIndex)
+                    return data;
+            }
+        }
+        if (levels.Count < buildIndex)
+        {
+            int missingLevels = buildIndex - levels.Count + 1;
+            for (int i = 0; i < missingLevels; i++)
+            {
+                LevelDataSer data = new LevelDataSer(i);
+                levels.Add(data);
+            }
+        }
+        LevelDataSer levelData = new LevelDataSer(buildIndex);
+        levels[buildIndex] = levelData;
+        return levelData;
+    }
     public GameData()
     {
         this.levels = new List<LevelDataSer>();
--- Assets/_Scripts/DataPersistence/Data/Types/LevelDataSer.cs
+++ Assets/_Scripts/DataPersistence/Data/Types/LevelDataSer.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class LevelDataSer 
+public class LevelDataSer
 {
-    public int buildIndex;
+    public int levelID;
     public float bestTime;
     public float bestStarCount;
-    
-    public LevelDataSer(int buildIndex, float bestTime, float bestStarCount)
-    {   
-        this.buildIndex = buildIndex;
-        this.bestTime = bestTime;   
+
+    public LevelDataSer(int levelID, float bestTime, float bestStarCount)
+    {
+        this.levelID = levelID;
+        this.bestTime = bestTime;
         this.bestStarCount = bestStarCount;
+    }
+    public LevelDataSer(int levelID)
+    {
+        this.levelID = levelID;
+        bestTime = 0;
+        bestStarCount = 0;
     }
 }
