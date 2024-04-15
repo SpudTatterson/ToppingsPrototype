bf7f9d39959ce59c72e84562673620a7874bbdcb
Aviv <avivhauyn@gmail.com>
2024-04-15 11:05:06 +0300

    Added base data class that all data types inherit from

--- Assets/_Scripts/DataPersistence/Data/GameData.cs
+++ Assets/_Scripts/DataPersistence/Data/GameData.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class SettingsData 
+public class Data
+{
+
+}
+[System.Serializable]
+public class SettingsData : Data
 {
     [Header("Sound")]
     public float masterVolume;
@@ -15,5 +20,15 @@ public class SettingsData
         this.masterVolume = 1;
         this.fxVolume = 1;
         this.musicVolume = 1;
+    }
+}
+[System.Serializable]
+public class GameData : Data
+{
+    public List<LevelDataSer> levels = new List<LevelDataSer>();
+
+    public GameData()
+    {
+        this.levels = new List<LevelDataSer>();
     }
 }
